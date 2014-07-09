577e5e432745797d082e6a768c2c0dbe2e96e33d
azangru <andrey.azov@gmail.com>
2014-07-09 22:59:31 +0100

    add a helper to remove annotations; styled

--- app/helpers/stanzas_helper.rb
+++ app/helpers/stanzas_helper.rb
@@ -1,29 +1,34 @@
 module StanzasHelper
 
-def put_tags_in_stanza(text)
-  # replace \n with line breaks
-  text.gsub!(/\n/, '<br/>')
+  def put_tags_in_stanza(text)
+    # replace \n with line breaks
+    text.gsub!(/\n/, '<br/>')
 
-  #replace italics pseudotags
-  text.gsub!("|||italics|||", "<em>")
-  text.gsub!("|||/italics|||", "</em>")
+    #replace italics pseudotags
+    text.gsub!("|||italics|||", "<em>")
+    text.gsub!("|||/italics|||", "</em>")
 
-  #replace small caps pseudotags
-  text.gsub!("|||smallcaps|||", "<span class='smallcaps'>")
-  text.gsub!("|||/smallcaps|||", "</span>")
+    #replace small caps pseudotags
+    text.gsub!("|||smallcaps|||", "<span class='smallcaps'>")
+    text.gsub!("|||/smallcaps|||", "</span>")
 
-  #get the information about the comment pseudotags
-  labels = text.scan(/(\|\|\|.*?\|\|\|)/) # gets an array of arrays of pseudotags such as |||asimov-4|||
-  numbers = text.scan(/\|\|\|asimov-(.*?)\|\|\|/) # gets an array of arrays of numbers of comments (such as: 4)
-  annotators = text.scan(/\|\|\|([a-zA-Z]*)-/) # gets an array of arrays of annotators (such as 'asimov')
+    #get the information about the comment pseudotags
+    labels = text.scan(/(\|\|\|.*?\|\|\|)/) # gets an array of arrays of pseudotags such as |||asimov-4|||
+    numbers = text.scan(/\|\|\|asimov-(.*?)\|\|\|/) # gets an array of arrays of numbers of comments (such as: 4)
+    annotators = text.scan(/\|\|\|([a-zA-Z]*)-/) # gets an array of arrays of annotators (such as 'asimov')
 
-  #step through the list of comment pseudotags and replace them with proper links
-  labels.each do |l| # an l is an array inside the array of arrays of pseudotags
-    text.gsub!("#{l.first}", "<a href='#' class='comment-link' data-comment-number='#{numbers[labels.index(l)].first}' data-annotator='#{annotators[labels.index(l)].first}'>*</a>")
+    #step through the list of comment pseudotags and replace them with proper links
+    labels.each do |l| # an l is an array inside the array of arrays of pseudotags
+      text.gsub!("#{l.first}", "<a href='#' class='comment-link' data-comment-number='#{numbers[labels.index(l)].first}' data-annotator='#{annotators[labels.index(l)].first}'>*</a>")
+    end
+
+    #return text with html tags and tell Rails to use these tags
+    text.html_safe
   end
 
-  #return text with html tags and tell Rails to use these tags
-  text.html_safe
-end
+  def remove_annotations(text)
+    # replace \n with line breaks
+    text.gsub!(/\|\|\|asimov-(.*?)\|\|\|/, '')
+  end
 
 end
